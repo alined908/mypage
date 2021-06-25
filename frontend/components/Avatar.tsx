e35faff6f6f7a347ee5491d19f5ef9ffe13efbdf
Daniel Lee <daniellee0228@berkeley.edu>
2021-06-25 02:47:07 -0700

    static twitter

--- frontend/components/Avatar.tsx
+++ frontend/components/Avatar.tsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/Header.module.scss';
-import axios from "axios";
 
 interface Profile {
     id: string
@@ -13,32 +12,11 @@ interface Profile {
 
 const Avatar = () => {
 
-    const [profile, setProfile] = useState<Profile>(null);
-
-    const getAvatar = async () => {
-        try {
-            const response = await axios.get('http://localhost:8000/api/avatar');
-            console.log(response);
-            const profile = response.data.body.data;
-            setProfile(profile);
-        } catch(e) {
-            console.log(e);
-        }
-    }
-
-    const processAvatar = () => {
-        return profile ? profile.profile_image_url.replace("_normal", "") : "/me.jpg"
-    }
-
-    useEffect(() => {
-        getAvatar()
-    }, [])
-
     return (
         <div className={styles.avatar}>
             <Link href="/">
                 <Image
-                    src={processAvatar()}
+                    src={"/tweeter.jpg"}
                     alt="Twitter Profile Pic"
                     width={60}
                     height={60}
